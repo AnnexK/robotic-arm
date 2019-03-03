a19881769c4d6209d0bf5bacd4e8f5bfd21c5c3c
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-03-03 20:03:27 +0700

    Rewritten the model of robotic arm to reflect changes in model

--- robot.py
+++ robot.py
@@ -10,32 +10,56 @@ class Vertex:
         return "({}; {}; {})".format(self.x, self.y, self.z)
         
 class Edge:
-    def __init__(self, length, phi, theta):
-        self.L = length
-        self.phi = phi
-        self.theta = theta
+    def __init__(self, length, len_l=None, len_r=None, angle=0.0, angle_l=None, angle_r=None):
+        if len_l == None:
+            len_l = length
+        if len_r == None:
+            len_r = length
+            
+        if len_l > len_r or length < len_l or length > len_r or len_l <= 0:
+            raise ValueError("Неверное значение длины или границ длины")
 
-    def __str__(self):
-        return "<L={}, phi={}, theta={}>".format(self.L, self.phi, self.theta)
+        if angle_l == None:
+            angle_l = angle
+        if angle_r == None:
+            angle_r = angle
+            
+        if angle_l < -pi or angle_r > pi or angle < angle_l or angle > angle_r or angle_l > angle_r:
+            raise ValueError("Неверное значение угла поворота")
+            
+        self.len = length
+        self.len_l = len_l
+        self.len_r = len_r
+        self.angle = angle
     
-    def rotate_z(self, angle):
-        self.phi += angle
+    def __str__(self):
+        return "<L=[{}; {} ({})], phi={}>".format(self.len_l, self.len_r, self.len, self.angle)
+    
+    def rotate(self, angle):
+        self.angle += angle
 
-    def rotate_xy(self, angle):
-        self.theta += angle
+    def expand(self, l):
+        if self.lenl <= self.length + l <= self.lenr:
+            self.length += l
+        else:
+            raise ValueError("Выход за границы длины")
 
         
 class RoboticArm:
-    """Класс, моделирующий поведение робота-манипулятора с хватом.
+    """Класс, моделирующий поведение робота-манипулятора с схватом.
     
     Положение манипулятора в пространстве определяется начальной
     точкой с координатами x, y, z и набором последовательно 
     соединенных звеньев, имеющих длину L, угол между звеном и осью
     Z phi и угол между звеном и плоскостью xOy theta."""
     
-    def __init__(self, x=0.0, y=0.0, z=0.0):
-        self.origin = Vertex(x, y, z)
-        self.edges = []
+    def __init__(self, x=0.0, y=0.0, z=0.0, angle=0.0, base_len=1.0):
+        try:
+            self.origin = Vertex(x, y, z)
+            self.edges = [Edge(base_len)]
+            self.angle = angle
+        except ValueError as E:
+            print(E)
 
     def __str__(self):
         ret = 'Edges:\n' + '\n'.join(list(map(lambda e: e.__str__(), self.edges)))
@@ -43,30 +67,36 @@ class RoboticArm:
         return ret
     
     """Добавляет звено"""
-    def append_edge(self, L, p, th):
-        self.edges.append(Edge(L, p, th))
+    def append_edge(self, L, ll=None, lr=None, angle=0.0, al=None, ar=None):
+        self.edges.append(Edge(L, ll, lr, angle, al, ar))
 
     """Возвращает положение сочленений звеньев"""
     def calculate_vertices(self):
         ret = [self.origin]
+        angle_z = 0.0
+        
+        cost = cos(self.angle)
+        sint = sin(self.angle)
+        
         for edge in self.edges:
-            new_x = ret[-1].x + edge.L*sin(edge.phi)*cos(edge.theta)
-            new_y = ret[-1].y + edge.L*sin(edge.phi)*sin(edge.theta)
-            new_z = ret[-1].z + edge.L*cos(edge.phi)
+            angle_z += edge.angle
+            
+            new_x = ret[-1].x + edge.len*sin(angle_z)*cost
+            new_y = ret[-1].y + edge.len*sin(angle_z)*sint
+            new_z = ret[-1].z + edge.len*cos(angle_z)
+            
             ret.append(Vertex(new_x, new_y, new_z))
         return ret
 
-    """Возвращает положение хвата (последнее звено)"""
+    """Возвращает положение схвата (последнее звено)"""
     def calculate_grip(self):
         return calculate_vertices()[-1]
 
-    def rotate_z(self, nedge, angle):
-        for edge in self.edges[nedge::1]:
-            edge.rotate_z(angle)
-
-    def rotate_xy(self, nedge, angle):
-        for edge in self.edges[nedge::1]:
-            edge.rotate_xy(angle)
+    def rotate_edge(self, nedge, angle):
+        self.edges[nedge].rotate(angle)
+        
+    def rotate(self, angle):
+        self.angle += angle
 
     """Параллельный перенос модели"""
     def move(self, dx, dy, dz):
@@ -76,9 +106,9 @@ class RoboticArm:
     
 if __name__ == "__main__":
     r = RoboticArm()
-    r.append_edge(2, pi/4, pi/2)
-    r.append_edge(2, 0, pi/2)
+    r.append_edge(L=2, angle=pi/4)
+    r.append_edge(L=2, angle=pi/2)
     print(r)
 
-    r.rotate_z(0, pi/4)
+    r.rotate(pi/2)
     print(r)

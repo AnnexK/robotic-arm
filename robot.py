4d75dbb64a99c7565509e2bd0313f72462f1d693
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-03-16 15:46:34 +0700

    Various fixes

--- robot.py
+++ robot.py
@@ -17,7 +17,7 @@ class Edge:
             len_r = length
             
         if len_l > len_r or length < len_l or length > len_r or len_l <= 0:
-            raise ValueError("Неверное значение длины или границ длины")
+            raise ValueError('Неверное значение длины или границ длины')
 
         if angle_l == None:
             angle_l = angle
@@ -25,33 +25,38 @@ class Edge:
             angle_r = angle
             
         if angle_l < -pi or angle_r > pi or angle < angle_l or angle > angle_r or angle_l > angle_r:
-            raise ValueError("Неверное значение угла поворота")
+            raise ValueError('Неверное значение угла поворота')
             
         self.len = length
         self.len_l = len_l
         self.len_r = len_r
         self.angle = angle
+        self.angle_l = angle_l
+        self.angle_r = angle_r
     
     def __str__(self):
-        return "<L=[{}; {} ({})], phi={}>".format(self.len_l, self.len_r, self.len, self.angle)
+        return '<L=[{}; {} ({})], phi=[{}; {} ({})]>'.format(self.len_l, self.len_r, self.len, self.angle_l, self.angle_r, self.angle)
     
     def rotate(self, angle):
-        self.angle += angle
+        if self.angle_l <= self.angle + angle <= self.angle_r:
+            self.angle += angle
+        else:
+            raise ValueError('Невозможно произвести наклон')
 
     def expand(self, l):
-        if self.lenl <= self.length + l <= self.lenr:
-            self.length += l
+        if self.len_l <= self.len + l <= self.len_r:
+            self.len += l
         else:
-            raise ValueError("Выход за границы длины")
+            raise ValueError('Невозможно произвести изменение длины')
 
         
 class RoboticArm:
     """Класс, моделирующий поведение робота-манипулятора с схватом.
     
     Положение манипулятора в пространстве определяется начальной
-    точкой с координатами x, y, z и набором последовательно 
-    соединенных звеньев, имеющих длину L, угол между звеном и осью
-    Z phi и угол между звеном и плоскостью xOy theta."""
+    точкой с координатами x, y, z, углом между плоскостью и осью OX
+    angle и набором последовательно соединенных звеньев, имеющих длину L
+    и угол между соседними ребрами angle."""
     
     def __init__(self, x=0.0, y=0.0, z=0.0, angle=0.0, base_len=1.0):
         try:
@@ -64,6 +69,7 @@ class RoboticArm:
     def __str__(self):
         ret = 'Edges:\n' + '\n'.join(list(map(lambda e: e.__str__(), self.edges)))
         ret = ret + '\nVertices:\n' + '\n'.join(list(map(lambda v: v.__str__(), self.calculate_vertices())))
+        ret = ret + '\nCurrent angle: {}'.format(self.angle)
         return ret
     
     """Добавляет звено"""
@@ -90,13 +96,29 @@ class RoboticArm:
 
     """Возвращает положение схвата (последнее звено)"""
     def calculate_grip(self):
-        return calculate_vertices()[-1]
+        return self.calculate_vertices()[-1]
 
     def rotate_edge(self, nedge, angle):
-        self.edges[nedge].rotate(angle)
-        
+        try:
+            self.edges[nedge].rotate(angle)
+        except ValueError as E:
+            print(E)
+            print('Номер ребра: {}'.format(nedge))
+            
+    def expand_edge(self, nedge, length):
+        try:
+            self.edges[nedge].expand(length)
+        except ValueError as E:
+            print(E)
+            print('Номер ребра: {}'.format(nedge))
+    
     def rotate(self, angle):
         self.angle += angle
+        if not -pi <= self.angle <= pi: # переписать
+            while self.angle < -pi:
+                self.angle += 2 * pi
+            while self.angle > pi:
+                self.angle -= 2 * pi                
 
     """Параллельный перенос модели"""
     def move(self, dx, dy, dz):
@@ -108,6 +130,11 @@ if __name__ == "__main__":
     r = RoboticArm()
     r.append_edge(L=2, angle=pi/4)
     r.append_edge(L=2, angle=pi/2)
+    r.append_edge(L=3, ll=1, al=0.0, ar=pi/2)
+    
+    print(r)
+    r.rotate_edge(1, pi)
+    
     print(r)
 
     r.rotate(pi/2)

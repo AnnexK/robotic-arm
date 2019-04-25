d22aedf0a5e9baf3954d8faacab4ab442418f9d1
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-04-26 00:13:48 +0700

    Added graph edge class, edge allocator for the graph, implemented a dictionary to store dictionaries indexed by orths, made grid step an attribute of an object rather than a class

--- gridgraph.py
+++ gridgraph.py
@@ -3,32 +3,51 @@ from math import inf
 from common_math import isclose_wrap
 
 
-def metric_static(*args):
-    # возвращает функцию, возвращающую константу
-    return lambda *a : args[0]
+def metric_static(args):
+    # возвращает 1
+    return 1.0
+
+class GraphEdge:
+    def __init__(self, w):
+        self._weight = w
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, value):
+        self._weight = value
 
 
 class GridGraph:
     """Класс, моделирующий граф-решетку в трехмерном пространстве"""
-    step = 0.1 # шаг сетки
-    def __init__(self, start_point, end_point, metfoo=metric_static(step)):
+    def __init__(self, start_point, end_point, grid_step=0.1, metfoo=metric_static, edge_type=GraphEdge):
         """Инициализировать граф с угловыми точками и заданной метрикой"""
 
+        if not issubclass(edge_type, GraphEdge):
+            raise ValueError("Передан класс не типа GraphEdge")
+
+        self.allocator = edge_type
+        
         # начальная и конечная точки
         self.start = [min(x, y) for x,y in zip(start_point,end_point)]
         self.end = [max(x, y) for x,y in zip(start_point,end_point)]
 
+        self.step = grid_step
+        
         # количество ребер в каждой размерности
-        self.x_nedges = int(abs(start_point[0] - end_point[0]) * (1/GridGraph.step))
-        self.y_nedges = int(abs(start_point[1] - end_point[1]) * (1/GridGraph.step))
-        self.z_nedges = int(abs(start_point[2] - end_point[2]) * (1/GridGraph.step))
+        self.x_nedges = int(abs(start_point[0] - end_point[0]) * (1/grid_step))
+        self.y_nedges = int(abs(start_point[1] - end_point[1]) * (1/grid_step))
+        self.z_nedges = int(abs(start_point[2] - end_point[2]) * (1/grid_step))
 
         # метрика графа
         self.metric = metfoo
 
         # X, Y, Z
-        self.edges = [SortedDict(), SortedDict(), SortedDict()]
+        self.edges = dict.fromkeys([(1,0,0),(0,1,0),(0,0,1)],SortedDict())        
 
+        
     def origin_to_grid(self, point):
         """origin_to_grid : tuple -> tuple
 Преобразует координаты точки в пространстве к координатам ближайшей к ней точки в графе
@@ -51,7 +70,7 @@ class GridGraph:
         in_z = lz<z<rz or isclose_wrap(lz,z) or isclose_wrap(rz,z)
 
         if in_x and in_y and in_z:
-            mult = 1/GridGraph.step
+            mult = 1/self.step
             return tuple(round(c * mult) for c in [x-lx, y-ly, z-lz])
         else:
             return -1,
@@ -60,7 +79,7 @@ class GridGraph:
         """grid_to_origin : tuple -> tuple/None
 Преобразует координаты сетки в координаты точки пространства
 Возвращает None, если преобразование невозможно"""
-        mult = 1/GridGraph.step
+        mult = 1/self.step
 
         if point == (-1,): # возвращается origin_to_grid
             return None
@@ -70,11 +89,11 @@ class GridGraph:
         in_z = 0 <= point[2] <= self.z_nedges
 
         if in_x and in_y and in_z:
-            return tuple(self.start[i] + GridGraph.step * point[i] for i in range(0,3))
+            return tuple(self.start[i] + self.step * point[i] for i in range(0,3))
         else:
             return None
 
-    def get_edge(self, start, end, *metric_data):
+    def get_edge(self, start, end, metric_data):
         """get_edge : tuple, tuple, (data) -> [float, float]
 Возвращает данные ребра с началом в точке start и концом в
 точке end (коорд. сетки)
@@ -87,17 +106,18 @@ class GridGraph:
 
         # выбор массива для поиска/вставки
         if abs(dx) == 1:
-            edges_idx = 0
+            edges_idx = (1,0,0)
         elif abs(dy) == 1:
-            edges_idx = 1
+            edges_idx = (0,1,0)
         elif abs(dz) == 1:
-            edges_idx = 2
+            edges_idx = (0,0,1)
         else:
-            edges_idx = -1
+            edges_idx = None
         
-        if abs(dx)+abs(dy)+abs(dz) != 1 or edges_idx < 0:
+        if abs(dx)+abs(dy)+abs(dz) != 1 or edges_idx is None:
             raise ValueError("Вектор не является ортом, параллельным осям")
 
+        # вычисление координат точки в решетке
         point = tuple(p - (v == -1) for p, v in zip(start, [dx,dy,dz])) 
 
         # не следует хранить в массивах точки, для которых нет ребра
@@ -110,9 +130,9 @@ class GridGraph:
             try:
                 return self.edges[edges_idx][point]
             except KeyError as ke: # элемента нет в ассоц. массиве
-                # длина + кол-во собственного феромона
-                edge = [self.metric(metric_data), 0.0]
+                # длина
+                edge = self.allocator(self.metric(metric_data))                
                 self.edges[edges_idx][point] = edge
                 return self.edges[edges_idx][point]
         else:
-            return (inf, 0.0) # ребра нет
+            return self.allocator(inf) # ребра нет

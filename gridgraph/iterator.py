fdc85657dd8556d7a9271d1d321acf99ed93a91a
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-06-26 20:29:29 +0700

    Added vertices iterator

--- gridgraph/iterator.py
+++ gridgraph/iterator.py
@@ -1,4 +1,4 @@
-class GGIterator:
+class EdgeIterator:
     def __init__(self, g):
         self.e = g.edges # ссылка на массив ребер
         self.ei = 0 # положение в массиве по параллельности ребер
@@ -26,3 +26,21 @@ class GGIterator:
                     self.zc = 0
                     self.ei += 1
         return self.e[eidx][idx]
+
+class VertIterator:
+    def __init__(self, g):
+        self.cur = -1
+        self.xm = g.x_nedges
+        self.ym = g.y_nedges
+        self.zm = g.z_nedges
+        self.c = self.ym * self.zm
+    
+    def __next__(self):
+        self.cur += 1
+        if self.cur == self.xm * self.ym * self.zm:
+            raise StopIteration
+        x = self.cur // self.c
+        y = (self.cur % self.c) // self.zm
+        z = self.cur % self.zm
+        return x, y, z
+        

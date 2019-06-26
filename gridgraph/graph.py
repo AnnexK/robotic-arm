7646a0a727f782aa60e65e33a434a0e8533ecb09
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-06-26 18:50:28 +0700

    Removed unnecessary methods & modified getters/setters

--- gridgraph/graph.py
+++ gridgraph/graph.py
@@ -1,5 +1,3 @@
-from math import inf
-from common_math import isclose_wrap
 from numpy import ndarray
 from functools import reduce
 
@@ -29,52 +27,6 @@ class GridGraph:
     def __iter__(self):
         return GGIterator(self)
 
-    # эти - отдельно
-    def origin_to_grid(self, point):
-        """origin_to_grid : tuple -> tuple
-Преобразует координаты точки в пространстве к координатам ближайшей к ней точки в графе
-Возвращает (-1,) если точка не находится в решетке"""
-        lx = self.start[0]
-        ly = self.start[1]
-        lz = self.start[2]
-
-        rx = self.end[0]
-        ry = self.end[1]
-        rz = self.end[2]
-
-        x = point[0]
-        y = point[1]
-        z = point[2]
-
-        # находится ли точка в решетке?
-        in_x = lx<x<rx or isclose_wrap(lx,x) or isclose_wrap(rx,x)
-        in_y = ly<y<ry or isclose_wrap(ly,y) or isclose_wrap(ry,y)
-        in_z = lz<z<rz or isclose_wrap(lz,z) or isclose_wrap(rz,z)
-
-        if in_x and in_y and in_z:
-            mult = 1/self.step
-            return tuple(int(round(c * mult)) for c in [x-lx, y-ly, z-lz])
-        else:
-            return -1,
-
-    def grid_to_origin(self, point):
-        """grid_to_origin : tuple -> tuple/None
-Преобразует координаты сетки в координаты точки пространства
-Возвращает None, если преобразование невозможно"""
-        mult = 1/self.step
-
-        if point == (-1,): # возвращается origin_to_grid
-            return None
-        
-        in_x = 0 <= point[0] <= self.x_nedges
-        in_y = 0 <= point[1] <= self.y_nedges
-        in_z = 0 <= point[2] <= self.z_nedges
-
-        if in_x and in_y and in_z:
-            return tuple(self.start[i] + self.step * point[i] for i in range(0,3))
-        else:
-            return None
-
     def __vert_in_graph(self, v):
         return 0 <= v[0] <= self.x_nedges and 0 <= v[1] <= self.y_nedges and 0 <= v[2] <= self.z_nedges
     
@@ -88,60 +40,28 @@ class GridGraph:
         # точки, в которые можно попробовать попасть
         return filter(self.__vert_in_graph, map(GridGraph.__sum_fun, [v]*6, vecs))
     
-    def __check_bounds(self, start, end):
-        """Проверка на принадлежность ребра графу"""
-        dx = abs(end[0]-start[0])
-        dy = abs(end[1]-start[1])
-        dz = abs(end[2]-start[2])
-
-        # выбор массива для поиска/вставки
-        if dx == 1:
-            edges_idx = 0
-        elif dy == 1:
-            edges_idx = 1
-        elif dz == 1:
-            edges_idx = 2
-        else:
-            return None, None
-        if dx+dy+dz != 1:
-            return None, None
-        
-        # вычисление координат точки в решетке
-        point = tuple(p - (v == -1) for p, v in zip(start, [dx,dy,dz])) 
-
-        # не следует хранить в массивах точки, для которых нет ребра
-        # поэтому вычитаем модули
-        in_x = 0 <= point[0] <= self.x_nedges - abs(dx)
-        in_y = 0 <= point[1] <= self.y_nedges - abs(dy)
-        in_z = 0 <= point[2] <= self.z_nedges - abs(dz)
-
-        if in_x and in_y and in_z:
-            return edges_idx, point
-        else:
-            return None, None
-        
     def __getitem__(self, v):
         start, end = v[0], v[1]
-        edges_idx, point = self.__check_bounds(start, end)
-        
-        if point is not None:
-            return self.edges[edges_idx][point]
+        if end in self.get_adjacent(start):
+            for i in range(3):
+                if abs(start[i]-end[i]) == 1:
+                    edges_idx = i
+                    break
+            return self.edges[edges_idx][min(start,end)]
         else:
             return None # ребра нет
 
     def __setitem__(self, v, value):
         start, end = v[0], v[1]
 
-        edges_idx, point = self.__check_bounds(start,end)
-
-        if point is not None:
-            try:
-                __ = iter(value)
-                self.edges[edges_idx][point] = self.allocator(*value)
-            except TypeError:
-                if value is None:
-                    self.edges[edges_idx][point] = self.allocator()
-                else:
-                    self.edges[edges_idx][point] = self.allocator(value)
+        if end in self.get_adjacent(start):
+            for i in range(3):
+                if abs(start[i]-end[i]) == 1:
+                    edges_idx = i
+                    break
+            if isinstance(value, tuple):
+                self.edges[edges_idx][min(start,end)] = self.allocator(*value)
+            else:
+                self.edges[edges_idx][min(start,end)] = None if value is None else self.allocator(value)
         else:
-            pass # или бросить исключение
+            raise KeyError("Вершины не смежны")

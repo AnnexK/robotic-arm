7c15f9434a343ea7b381d5fe3eba55da0d89a109
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-06-28 10:53:56 +0700

    Modified conditions

--- simple_test.py
+++ simple_test.py
@@ -1,8 +1,9 @@
 import solver
+import csv
 
 print('Building graph...')
-G = solver.ExampleGraphBuilder(50,50,50).make_graph()
-S = solver.AntSolver(G, (1,2,3), (4,4,4), q=1000.0)
+G = solver.ExampleGraphBuilder(120,120,120).make_graph()
+S = solver.AntSolver(G, (1,2,3), (12,14,24), q=1000.0)
 print('Solving...')
 best, worst, avg = S.solve(iters=20, ants_n=10)
 print(best, worst, avg)

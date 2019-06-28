b7ef0af84f9ab8ae99a25949bb2e15ab1283b824
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-06-28 10:54:24 +0700

    Modified main function with accordance to changes in Solver

--- cli/__init__.py
+++ cli/__init__.py
@@ -10,8 +10,12 @@ def main():
     parser = make_parser()
     args = parser.parse_args()
     
-    Robot, Env, End = env.load_task(pathlib.Path(args.task))
-    S = solver.AntSolver(Robot, End, args.alpha, args.beta, args.ant_power, args.decay, args.phi)
+    print('Loading task...')
+    Robot, Env, End, emp_best = env.load_task(pathlib.Path(args.task))
+    print('Building graph...')
+    G, start, end = solver.GraphBuilder(Robot, Env, End).make_graph()
+    S = solver.AntSolver(G, start, end, args.alpha, args.beta, args.ant_power, args.decay, args.phi)
+    print('Solving...')
     best, worst, avg = S.solve(args.iters, args.ant_num)
 
     with open(args.output, 'w', newline='') as out:
@@ -19,3 +23,5 @@ def main():
         writer.writerow(best)
         writer.writerow(worst)
         writer.writerow(avg)
+        # нижний предел
+        writer.writerow([emp_best] * args.iters)

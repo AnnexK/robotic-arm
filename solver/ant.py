4b31ce2815afe05a84a0c45643f090138b1101f0
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-07-03 15:46:53 +0700

    Reimplemented path to be able to remove cycles

--- solver/ant.py
+++ solver/ant.py
@@ -5,6 +5,42 @@ def edge_attraction(phi, dist, alpha, beta):
     """Вычисляет привлекательность ребра по формуле"""
     return phi ** alpha * (1 / dist) ** beta
 
+class AntPath:
+    class PathNode:
+        def __init__(self, data=None, prev=None, next=None):
+            self.d = data
+            self.next = self if next is None else next
+            self.prev = self if prev is None else prev
+
+        def is_sentinel(self):
+            return self.d is None
+        
+    def __init__(self, v):
+        self.sent = AntPath.PathNode()
+        self.sent.next = AntPath.PathNode(v, self.sent, self.sent)
+        self.sent.prev = self.sent.next
+
+    @property
+    def start(self):
+        return self.sent.next
+
+    @property
+    def end(self):
+        return self.sent.prev
+
+    def append(self, v):
+        new = AntPath.PathNode(v, self.end, self.sent)
+        self.end.next = new
+        self.sent.prev = new
+    
+    def extract(self, start, end):
+        start.prev.next = end.next
+        end.next.prev = start.prev
+
+    def clear(self):
+        self.sent.prev = self.sent
+        self.sent.next = self.sent
+    
 class Ant:
     """Класс, моделирующий поведение муравья"""
     # коэф привлекательности по феромону
@@ -20,9 +56,7 @@ class Ant:
     
     def __init__(self, pos):
         self.pos = pos # текущее положение в сетке
-        self.path_len = 0.0 # длина текущего пути
-        self.path_edges = [] # стек с ребрами
-        self.path_verts = [pos] # стек с вершинами
+        self.path = AntPath(pos)
 
     @property
     def pos(self):
@@ -37,6 +71,15 @@ class Ant:
         if not Ant.assoc_graph.vert_in_graph(value):
             raise ValueError("Точка не принадлежит графу")
         self._grid_pos = value
+
+    @property
+    def path_len(self):
+        ret = 0.0
+        n = self.path.start
+        while n != self.path.end:
+            ret += self.assoc_graph[n.d, n.next.d].weight
+            n = n.next
+        return ret
 
     def pick_edge(self):
         """Совершает перемещение в одну из соседних точек в графе G"""
@@ -61,29 +104,34 @@ class Ant:
         # случайный выбор тут
         choice = random.choice(len(edges), p=attr)
         
-        # добавить в стек
-        self.path_edges.append(edges[choice]) 
-        self.path_verts.append(targets[choice])
-        self.path_len += edges[choice].weight
+        # добавить в путь
+        self.path.append(targets[choice])
         self.pos = targets[choice]
+
+    def remove_cycles(self):
+        """Извлекает циклы из пути"""
+        cur_left = self.path.start
+        while cur_left != self.path.end:
+            cur_right = self.path.end
+            while cur_right != cur_left:
+                if cur_left.d == cur_right.d:
+                    self.path.extract(cur_left.next, cur_right)
+                    break
+                cur_right = cur_right.prev
+            cur_left = cur_left.next
 
     def distribute_pheromone(self):
         """Распространяет феромон по всем пройденным ребрам"""
         phi = self.Q / self.path_len
         
-        for e in self.path_edges:
-            e.phi = e.phi + phi
+        n = self.path.start
+        while n != self.path.end:
+            self.assoc_graph[n.d,n.next.d].phi += phi
+            n = n.next
 
     def unwind_path(self):
-        """Возвращает муравья к начальному состоянию и возвращает
-длину пройденного им пути"""
-        ret = self.path_len
+        """Возвращает муравья к начальному состоянию"""
 
-        self.pos = self.path_verts[0]
-        self.path_verts.clear()
-        self.path_verts.append(self.pos)
-        
-        self.path_edges.clear()
-        self.path_len = 0.0
-        
-        return ret
+        self.pos = self.path.start.d
+        self.path.clear()
+        self.path.append(self.pos)

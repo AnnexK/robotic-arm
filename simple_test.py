aa00f76bee6c9c766cc70af3790c30fc9c4141a0
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-07-05 01:06:43 +0700

    Output file now closes properly

--- simple_test.py
+++ simple_test.py
@@ -7,15 +7,15 @@ G = solver.ExampleGraphBuilder(30,30,30).make_graph()
 S = solver.AntSolver(G, (10,2,3), (15,22,25),
                      a=1.0,
                      b=1.0,
-                     q=5.0,
+                     q=500.0,
                      rho=0.01,
                      base_pheromone=0.01)
 
 print('Solving...')
-best, worst, avg = S.solve(iters=50, ants_n=10)
+best, worst, avg = S.solve(iters=50, ants_n=64)
 
 name = input('Input file name to save:')
 
 w = writer.ColumnWriter(open(name, 'w', newline=''), ' ')
 w.write(best,worst,avg)
-
+del w # закрытие файла

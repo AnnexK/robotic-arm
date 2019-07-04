2057e98baa03c0acafdfba7a59f5827d48646a8d
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-07-04 11:57:03 +0700

    Now uses new writer package

--- simple_test.py
+++ simple_test.py
@@ -1,21 +1,20 @@
 import solver
-import cli.csv_writer as writer
+import writer
 
 print('Building graph...')
-G = solver.ExampleGraphBuilder(12,12,12).make_graph()
+G = solver.ExampleGraphBuilder(30,30,30).make_graph()
 
-S = solver.AntSolver(G, (1,2,3), (7,8,8),
+S = solver.AntSolver(G, (10,2,3), (15,22,25),
                      a=1.0,
                      b=1.0,
-                     q=1000.0,
+                     q=5.0,
                      rho=0.01,
                      base_pheromone=0.01)
 
 print('Solving...')
 best, worst, avg = S.solve(iters=50, ants_n=10)
 
-print('Input file name to save:')
-name = input()
+name = input('Input file name to save:')
 
 w = writer.ColumnWriter(open(name, 'w', newline=''), ' ')
 w.write(best,worst,avg)

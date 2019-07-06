5b71de6b0ab751dba6dfbe4249892418c337807f
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-07-06 12:35:13 +0700

    Removed common_math due to it being unused

--- common_math.py
+++
@@ -1,6 +0,0 @@
-from math import isclose
-
-def isclose_wrap(a, b):
-    """Обертка для сравнения двух чисел с плавающей точкой"""
-    eps = 1e-9 # можно менять
-    return isclose(a, b, abs_tol=eps)

5cf71a2633cc04cb0fbb8b0bbb679ea42e9d5117
TrotzigerEmil <roadto3kmmr@gmail.com>
2019-07-06 12:48:51 +0700

    Rewrote simple test with accordance to changes

--- simple_test.py
+++ simple_test.py
@@ -2,14 +2,16 @@ import solver
 import writer
 
 print('Building graph...')
-G = solver.ExampleGraphBuilder(30,30,30).make_graph()
+G, start, end = solver.ExampleGraphBuilder(30,30,30,
+                                           s=(10,2,3),
+                                           e=(15,22,25),
+                                           base=0.01).make_graph()
 
-S = solver.AntSolver(G, (10,2,3), (15,22,25),
+S = solver.AntSolver(G, start, end,
                      a=1.0,
                      b=1.0,
                      q=500.0,
-                     rho=0.01,
-                     base_pheromone=0.01)
+                     rho=0.01)
 
 print('Solving...')
 best, worst, avg = S.solve(iters=50, ants_n=64)

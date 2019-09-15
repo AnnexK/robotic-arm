a0806e177ce48b745e7a85168bfccd8d9a5a630e
frolikk <roadto3kmmr@gmail.com>
2019-09-15 19:46:32 +0700

    Rewrote writers to use context manager protocol

--- writer/csv_writer.py
+++ writer/csv_writer.py
@@ -1,26 +1,38 @@
 import csv
 
+
 class DefaultWriter:
-    def __init__(self, fp, delim):
-        self.fp = fp
+    def __init__(self, filename, delim):
+        self.fp = open(filename, mode='w')
         self.delimiter = delim
+
+    def __enter__(self):
+        return self
 
     def write(self, *data):
         writer = csv.writer(self.fp, delimiter=self.delimiter)
         writer.writerows(data)
 
-    def __del__(self):
+    def __exit__(self, type, value, traceback):
         self.fp.close()
+        if value:
+            raise
+
 
 class ColumnWriter:
-    def __init__(self, fp, delim):
-        self.fp = fp
+    def __init__(self, filename, delim):
+        self.fp = open(filename, mode='w')
         self.delimiter = delim
+
+    def __enter__(self):
+        return self
 
     def write(self, *data):
         writer = csv.writer(self.fp, delimiter=self.delimiter)
         # номер итерации и данные в каждой строке
         writer.writerows(zip(range(1, len(data[0])+1), *data))
 
-    def __del__(self):
+    def __exit__(self, type, value, traceback):
         self.fp.close()
+        if value:
+            raise

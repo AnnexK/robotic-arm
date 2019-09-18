3a3b7146e2c36119a76115b4d08368ce9bca2105
frolikk <roadto3kmmr@gmail.com>
2019-09-18 15:30:21 +0700

    Added robot graph builder

---
+++ solver/builders/robot_builder.py
@@ -0,0 +1,19 @@
+from gridgraph.graph import GridGraph
+from gridgraph.weight import RobotWeight
+
+
+class RoboticGraphBuilder:
+    def __init__(self, r, end, base):
+        self.robot = r
+        self.phi = base
+        self.end = end
+
+    def make_graph(self):
+        ret = GridGraph(
+            RobotWeight(self.robot), self.phi)
+
+        origin = self.robot.get_effector()
+        diff = tuple(self.end[i] - origin[i] for i in range(3))
+        endpoint = tuple(round(diff[i]) for i in range(3))
+
+        return ret, (0, 0, 0), endpoint

0316d634f6316667ebf12db8b97e16bf8c28b00d
frolikk <roadto3kmmr@gmail.com>
2019-09-18 15:34:41 +0700

    Rewrote main function with accordance to changes'

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,10 +1,16 @@
 from cli.arg_parser import make_parser, check_args
 
 import env
-import solver
-import csv
+
+from solver.builders.robot_builder import RoboticGraphBuilder
+from solver.ants.ant import Ant
+from solver.ants.rob_decorator import RobotizedAnt
+from solver.aco_algorithms.simple_aco import SimpleACO
+from solver.solver import AntSolver
+
 import pathlib
 import writer
+
 
 def main():
     """Основная функция программы"""
@@ -12,17 +18,26 @@ def main():
     args = parser.parse_args()
     if not check_args(args):
         raise ValueError('one of the args has wrong value')
-        
+
     print('Loading task...')
     Robot, Env, End, emp_best = env.load_task(pathlib.Path(args.task))
     print('Building graph...')
-    G, start, end = solver.GraphBuilder(Robot, Env, End, args.phi).make_graph()
-    strat = solver.SimpleACO(G, start, end, args.alpha, args.beta, args.ant_power, args.decay)
-    S = solver.AntSolver(strat)
-    
+    G, start, end = RoboticGraphBuilder(
+        Robot, End, args.phi).make_graph()
+
+    a = Ant(args.alpha,
+            args.beta,
+            args.ant_power,
+            G,
+            start)
+    rob_ant = RobotizedAnt(a, Robot)
+
+    strat = SimpleACO(G, end, args.decay)
+    strat.set_proto(rob_ant)
+
+    S = AntSolver(strat)
     print('Solving...')
     best, worst, avg = S.solve(args.iters, args.ant_num)
 
-    w = writer.ColumnWriter(open(args.output, 'w', newline=''), ' ')
-    w.write(best, worst, avg, [emp_best] * args.iters)
-    del w # закрыть связанный файл
+    with writer.ColumnWriter(input('Input save file name: '), ' ') as w:
+        w.write(best, worst, avg)

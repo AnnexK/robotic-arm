e6ffbee1dd438533ed73a63420c4d15fef7fb265
frolikk <roadto3kmmr@gmail.com>
2019-09-21 00:01:12 +0700

    Added path writing

--- simple_test.py
+++ simple_test.py
@@ -3,7 +3,7 @@ from solver.ants.ant import Ant
 from solver.aco_algorithms.simple_aco import SimpleACO
 from solver.solver import AntSolver
 
-from solver.graph_builder import ExampleGraphBuilder
+from solver.builders.graph_builder import ExampleGraphBuilder
 
 print('Building graph...')
 G, start, end = ExampleGraphBuilder(
@@ -18,7 +18,10 @@ S.set_proto(a)
 
 sol = AntSolver(S)
 print('Solving...')
-best, worst, average = sol.solve(iters=50, ants_n=64)
+best, worst, average, path = sol.solve(iters=5, ants_n=64)
 
 with writer.ColumnWriter('simple.csv', ' ') as w:
     w.write(best, worst, average)
+
+with writer.DefaultWriter('result.txt', ' ') as w:
+    w.write(path)

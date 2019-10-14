03042e0d3600c8ab26a9709bdabc56f426e9d7dd
frolikk <roadto3kmmr@gmail.com>
2019-10-14 12:12:34 +0700

    Now sends RPY to Robot init && various pep8 fixes

--- env/__init__.py
+++ env/__init__.py
@@ -5,28 +5,31 @@ import pathlib
 from env.robot import Robot
 from env.environment import Environment
 
+server_type = pybullet.DIRECT
 # присоединиться к серверу при подключении пакета
-server_id = pybullet.connect(pybullet.DIRECT)
+server_id = pybullet.connect(server_type)
 if server_id > 0:
     print("default pb server already connected")
     pybullet.disconnect(server_id)
 
+
 def task_json_parse(dct):
     """Добавляет к прочитанному из json словарю значения по умолчанию,
 если они не были предоставлены"""
-    if not 'pos' in dct:
-        dct['pos'] = (.0,.0,.0)
-    if not 'orn' in dct:
-        dct['orn'] = (0.0, 0.0, 0.0) # rpy
-    if not 'eps' in dct:
-        dct['eps'] = 1e-4 # 0.1 mm
-    if not 'fixed_base' in dct:
+    if 'pos' not in dct:
+        dct['pos'] = (.0, .0, .0)
+    if 'orn' not in dct:
+        dct['orn'] = (0.0, 0.0, 0.0)  # rpy
+    if 'eps' not in dct:
+        dct['eps'] = 1e-4  # 0.1 mm
+    if 'fixed_base' not in dct:
         dct['fixed_base'] = True
-    if not 'sdf_name' in dct:
+    if 'sdf_name' not in dct:
         dct['sdf_name'] = None
-    if not 'dofs' in dct:
+    if 'dofs' not in dct:
         dct['dofs'] = None
     return dct
+
 
 def load_task(filename):
     """Загружает параметры задачи из json-файла filename"""
@@ -34,30 +37,25 @@ def load_task(filename):
         task_data = json.load(fp, object_hook=task_json_parse)
 
     # получение путей к urdf и sdf файлам
-    
+
     urdf_filename = filename.parent / pathlib.Path(task_data['urdf_name'])
     if task_data['sdf_name'] is None:
         sdf_filename = None
     else:
         sdf_filename = filename.parent / pathlib.Path(task_data['sdf_name'])
 
-    # преобразование считанных из файла RPY углов в кват-н
-    q_orn = pybullet.getQuaternionFromEuler(task_data['orn'])
-
     # создание оберток
     R = Robot(str(urdf_filename),
               task_data['effector_name'],
               task_data['pos'],
-              q_orn,
+              task_data['orn'],
               task_data['fixed_base'],
               task_data['eps'])
 
     # задание начального состояния звеньев
     if task_data['dofs'] is not None:
-        for i, dof in enumerate(R._dofs):
-            pybullet.resetJointState(R.id, dof, task_data['dofs'][i])
+        R.state = task_data['dofs']
 
     E = Environment(None if sdf_filename is None else str(sdf_filename))
 
     return R, E, task_data['endpoint'], task_data['emp_best']
-

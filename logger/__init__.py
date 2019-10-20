ef89c0423af70bad5ec9ee928fc82b606abb0727
frolikk <roadto3kmmr@gmail.com>
2019-10-20 21:42:19 +0700

    Added loggers initialization

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,5 +1,8 @@
 from .logger_collection import LoggerCollection as log
 from sys import stdout
 
-
+log().add_logger('SOLVER', stdout)
 log().add_logger('ANT', stdout)
+log().add_logger('PYBULLET', stdout)
+
+log().log('loggers loaded normally')

876691eadf3660be38a5aa78fde9b47f4950b1cf
frolikk <roadto3kmmr@gmail.com>
2019-10-23 18:06:22 +0700

    Added memoizing weight calculation results

--- gridgraph/weight_calcs/robotic.py
+++ gridgraph/weight_calcs/robotic.py
@@ -6,15 +6,25 @@ class RobotWeight:
     def __init__(self, R: Robot):
         self.origin = R.get_effector()
         self.R = R
+        # посчитанные веса ребер
+        self.computed_lengths = {}
 
     def get(self, v, w):
         def moved_normally(R, target):
             return R.move_to(target) and not R.check_collisions()
 
-        w_real = tuple(self.origin[i] + self.R.kin_eps * w[i] for i in range(3))
+        key = (min(v, w), max(v, w))
 
-        state = self.R.state
-        ret = 1.0 if moved_normally(self.R, w_real) else inf
-        self.R.state = state
+        # не посчитано
+        if key not in self.computed_lengths:
+            w_real = tuple(self.origin[i] + self.R.kin_eps * w[i]
+                           for i in range(3))
+            state = self.R.state
+            ret = 1.0 if moved_normally(self.R, w_real) else inf
+            self.computed_lengths[key] = ret
+            self.R.state = state
+        # посчитано
+        else:
+            ret = self.computed_lengths[key]
 
         return ret

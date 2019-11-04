bb4b0c64a9a563fd41c33e232e34b1fcbb580f74
frolikk <roadto3kmmr@gmail.com>
2019-11-04 20:34:35 +0700

    Removed server autoconnect on module load.

--- env/__init__.py
+++ env/__init__.py
@@ -1,20 +1,8 @@
 import json
-import pybullet
 import pathlib
 
-from env.robot import Robot
 from env.environment import Environment
 from logger import log
-
-server_type = pybullet.GUI
-# присоединиться к серверу при подключении пакета
-server_id = pybullet.connect(server_type, options='--opengl2')
-if server_id == -1:
-    raise RuntimeError('not connected to pb server')
-
-if server_id > 0:
-    log()['PYBULLET'].log('already connected to server with id=0')
-    pybullet.disconnect(server_id)
 
 
 def task_json_parse(dct):
@@ -48,22 +36,20 @@ def load_task(filename):
     else:
         sdf_filename = filename.parent / pathlib.Path(task_data['sdf_name'])
 
-    # создание оберток
-    R = Robot(str(urdf_filename),
-              task_data['effector_name'],
-              task_data['pos'],
-              task_data['orn'],
-              task_data['fixed_base'],
-              task_data['eps'])
+    E = Environment(filename=None if sdf_filename is None
+                    else str(sdf_filename))
 
-    log()['PYBULLET'].log('urdf loaded successfully')
+    E.set_endpoint(task_data['endpoint'])
+    E.add_robot(filename=str(urdf_filename),
+                eff_name=task_data['effector_name'],
+                pos=task_data['pos'],
+                orn=task_data['orn'],
+                fixed=task_data['fixed_base'],
+                kin_eps=task_data['eps'])
 
-    # задание начального состояния звеньев
     if task_data['dofs'] is not None:
-        R.state = task_data['dofs']
-
-    E = Environment(None if sdf_filename is None else str(sdf_filename))
+        E.robot.state = task_data['dofs']
 
     log()['PYBULLET'].log('environment loaded successfully')
 
-    return R, E, task_data['endpoint'], task_data['emp_best']
+    return E, task_data['endpoint'], task_data['emp_best']

5cd3d5923dbb5d21295e06e021b8c0cde2f415d4
frolikk <roadto3kmmr@gmail.com>
2019-11-04 20:29:00 +0700

    Removed memoization. Now returns accuracy (basically grid step).

--- gridgraph/weight_calcs/robotic.py
+++ gridgraph/weight_calcs/robotic.py
@@ -6,25 +6,16 @@ class RobotWeight:
     def __init__(self, R: Robot):
         self.origin = R.get_effector()
         self.R = R
-        # посчитанные веса ребер
-        self.computed_lengths = {}
+        self.w = R.kin_eps
 
     def get(self, v, w):
         def moved_normally(R, target):
             return R.move_to(target) and not R.check_collisions()
 
-        key = (min(v, w), max(v, w))
-
-        # не посчитано
-        if key not in self.computed_lengths:
-            w_real = tuple(self.origin[i] + self.R.kin_eps * w[i]
-                           for i in range(3))
-            state = self.R.state
-            ret = 1.0 if moved_normally(self.R, w_real) else inf
-            self.computed_lengths[key] = ret
-            self.R.state = state
-        # посчитано
-        else:
-            ret = self.computed_lengths[key]
+        w_real = tuple(self.origin[i] + self.R.kin_eps * w[i]
+                       for i in range(3))
+        state = self.R.state
+        ret = self.w if moved_normally(self.R, w_real) else inf
+        self.R.state = state
 
         return ret

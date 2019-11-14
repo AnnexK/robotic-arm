de2a3916ccd9fcce120dfddf4a309344299fdfdd
frolikk <roadto3kmmr@gmail.com>
2019-11-14 18:09:59 +0700

    Requires FPS instead of time per frame

--- seq-viewer.py
+++ seq-viewer.py
@@ -4,14 +4,24 @@ import pathlib
 from time import sleep
 
 
+def make_sequence(S):
+    ret = []
+    for s in S:
+        ret.append(list(float(q) for q in s[1:-3].split(',')))
+    return ret
+
+
 p = argparse.ArgumentParser()
 p.add_argument('task')
 p.add_argument('seq')
-p.add_argument('-t', type=float, default=0.5)
+p.add_argument('--fps', type=int, default=5)
 p.add_argument('--fallback', action='store_true')
 
 args = p.parse_args()
+if args.fps <= 0:
+    raise ValueError('FPS too small')
 
+slp = 1/args.fps
 E, end, emp = env.load_task(pathlib.Path(args.task),
                             render=True,
                             fallback=args.fallback)
@@ -21,15 +31,13 @@ R = E.robot
 with open(args.seq, 'r') as seqfile:
     S = seqfile.readlines()
 
-Q = []
-for s in S:
-    Q.append(list(float(q) for q in s[1:-3].split(',')))
+Q = make_sequence(S)
 
 for q in Q:
-    sleep(args.t)
+    sleep(slp)
     R.state = q
-    print(R.check_collisions())
 
 input()
 
+del R
 del E

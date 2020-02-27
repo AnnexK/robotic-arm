0fe879277da11677fcf642d6c18f7b4f9d6c7cc3
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-02-27 20:47:32 +0700

    Logging additions

--- gridgraph/weight_calcs/robotic.py
+++ gridgraph/weight_calcs/robotic.py
@@ -1,5 +1,6 @@
 from env.robot import Robot
 from math import inf
+from logger import log
 
 
 class RobotWeight:
@@ -10,12 +11,20 @@ class RobotWeight:
 
     def get(self, v, w):
         def moved_normally(R, target):
-            return R.move_to(target) and not R.check_collisions()
+            m = R.move_to(target)
+            col = R.check_collisions()
+            if col:
+                log()['COLLISION'].log('collision detected')
+            return m and not col
 
         w_real = tuple(self.origin[i] + self.R.kin_eps * w[i]
                        for i in range(3))
-        state = self.R.state
-        ret = self.w if moved_normally(self.R, w_real) else inf
-        self.R.state = state
+        try:
+            state = self.R.state
+            ret = self.w if moved_normally(self.R, w_real) else inf
+            self.R.state = state
+        except ValueError:
+            log()['FATAL'].log(f'reset state error, state was {state}')
+            raise
 
         return ret

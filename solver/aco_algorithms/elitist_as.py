ef133bfa32bff9d85dea2bee17b960d9eae8328d
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-02-28 23:54:29 +0700

    Fixed pheromone addition

--- solver/aco_algorithms/elitist_as.py
+++ solver/aco_algorithms/elitist_as.py
@@ -1,5 +1,5 @@
 from .ant_system import AntSystem
-
+from logger import log
 
 class ElitistAS(AntSystem):
     def __init__(self, G, Q, end, decay, Q_e):
@@ -8,8 +8,10 @@ class ElitistAS(AntSystem):
 
     def update_pheromone(self, ants):
         super().update_pheromone(ants)
-        phi = self.elite_power / self.best_solution['length']
+        path_len = self.best_solution['length']
+        phi = self.elite_power / path_len
 
+        log()['EAS'].log(f'depositing {phi} pheromone on len={path_len}')
         for v, w in zip(self.best_solution['path'][:-1],
                         self.best_solution['path'][1:]):
-            self.graph.add_phi(v, w, phi)
+            self.graph.add_phi(v[0], w[0], phi)

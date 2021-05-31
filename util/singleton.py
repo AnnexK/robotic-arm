b2a6fb4fc32c64630da8d8f65ed5cc5415d701cf
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-01 03:25:47 +0700

    Singleton moved to util package

--- util/singleton.py
+++ util/singleton.py
@@ -1,5 +1,8 @@
+from typing import Dict, Any
+
+
 class Singleton(type):
-    _instances = {}
+    _instances: Dict[Any, Any] = {}
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:

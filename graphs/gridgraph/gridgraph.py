18d14e2e994831807155b46ea2c8f43b7b65d9a6
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 02:37:44 +0700

    typing, renaming

--- graphs/gridgraph/gridgraph.py
+++ graphs/gridgraph/gridgraph.py
@@ -1,9 +1,17 @@
-class GridGraph:
+from typing import Iterable
+from ..phigraph import PhiGraph
+from .wg_managers.manager import WeightManager
+from .phi_managers.manager import PhiManager
+from .vertex import GGVertex
+
+
+class GridGraph(PhiGraph[GGVertex]):
     """Класс, моделирующий граф-решетку в трехмерном пространстве"""
-    def __sum_fun(x, y):
+    @staticmethod
+    def __sum_fun(x: GGVertex, y: GGVertex) -> GGVertex:
         return x[0]+y[0], x[1]+y[1], x[2]+y[2]
 
-    def __init__(self, weight, phi):
+    def __init__(self, weight: WeightManager, phi: PhiManager):
         """
         Инициализировать граф
         с весовой функцией weight
@@ -15,7 +23,7 @@ class GridGraph:
         # объект, управляющий уровнем феромона
         self.phi = phi
 
-    def get_adjacent(self, v):
+    def get_adjacent(self, v: GGVertex) -> Iterable[GGVertex]:
         """Возвращает список смежных вершин для вершины v"""
         vecs = [(1, 0, 0),
                 (-1, 0, 0),
@@ -27,26 +35,26 @@ class GridGraph:
         # точки, в которые можно попробовать попасть
         return map(GridGraph.__sum_fun, [v]*6, vecs)
 
-    def get_weight(self, v, w):
+    def get_weight(self, v: GGVertex, w: GGVertex) -> float:
         '''Вычисляет вес ребра между вершинами v и w'''
-        return self.weight.get(v, w)
+        return self.weight.get_weight(v, w)
 
-    def get_phi(self, v, w):
+    def get_phi(self, v: GGVertex, w: GGVertex) -> float:
         '''Возвращает кол-во феромона между вершинами v и w'''
         if w in self.get_adjacent(v):
             return self.phi.get_phi(v, w)
         else:
             raise ValueError('Vertices not adjacent')
 
-    def add_phi(self, v, w, val):
+    def add_phi(self, v: GGVertex, w: GGVertex, val: float):
         '''Добавляет val феромона на ребро между v и w'''
         if w in self.get_adjacent(v):
             self.phi.add_phi(v, w, val)
         else:
             raise ValueError('Vertices not adjacent')
 
-    def evaporate(self, value):
+    def evaporate(self, rate: float):
         '''Производит испарение феромона на всем графе'''
-        if not (0 <= value <= 1):
+        if not (0.0 <= rate <= 1.0):
             raise ValueError('Decay rate not in bounds (0 < rho < 1)')
-        self.phi.evaporate(value)
+        self.phi.evaporate(rate)

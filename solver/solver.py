223288c2c8108d083ab9b007a34d0d1957d48377
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 19:16:21 +0700

    Typing, plotter changes

--- solver/solver.py
+++ solver/solver.py
@@ -1,47 +1,49 @@
+from plotter.plotter import PlotterLink
 from logger import log
-from plotter.plottable import Plottable
-from plotter.mux import Mux, PMLink
+from plotter.plottable import Plottable, Link
 from math import inf
+from .aco_algorithms import ACOAlgorithm
+from .ants import BaseAnt
+from typing import Generic, Iterable, TypeVar
 
 
 import traceback
 
 
-class AntSolver(Plottable):
+V = TypeVar('V')
+
+
+class AntSolver(Generic[V], Plottable):
     """Класс, моделирующий решение задачи"""
-    def __init__(self, strat, ant):
+    def __init__(self, strat: ACOAlgorithm[V], ant: BaseAnt[V]):
         self.S = strat
         self.p_ant = ant
-        self.min_iter = PMLink('min')
-        self.max_iter = PMLink('max')
-        self.avg_iter = PMLink('avg')
-        Mux().register_plottable_link(self,self.min_iter)
-        Mux().register_plottable_link(self,self.max_iter)
-        Mux().register_plottable_link(self,self.avg_iter)
-        self.min_iter.y = inf
-
-    def __del__(self):
-        Mux().deregister_plottable(self)
+        self.min_iter = Link()
+        self.max_iter = Link()
+        self.avg_iter = Link()
+        self.min_iter.set_point(0.0, inf)
     
-    def solve(self, iters=1, ants_n=20):
-        i = 1
-        repeats = 0
+    def get_links(self) -> Iterable[PlotterLink]:
+        return (self.min_iter, self.max_iter, self.avg_iter)
+    
+    def solve(self, iters: int, ants_n: int):
+        i: int = 1
+        repeats: int = 0
         try:
             while i <= iters:
-                ants = [self.p_ant.clone() for i in range(ants_n)]
+                ants = [self.p_ant.clone() for _ in range(ants_n)]
                 log()['SOLVER'].log(f'Iter {i}')
                 self.S.generate_solutions(ants)
                 if not any(a.complete for a in ants):
                     log()['SOLVER'].log(f'No results on iter {i}, restarting')
                 else:
                     prev_avg = self.avg_iter.y
-                    self.min_iter.y = min(self.min_iter.y, min(a.path_len for a in ants))
-                    self.max_iter.y = max(a.path_len for a in ants)
-                    self.avg_iter.y = sum(a.path_len for a in ants) / len(ants)
-                    self.min_iter.x = self.max_iter.x = self.avg_iter.x = float(i)
-                    self.announce_point(self.min_iter)
-                    self.announce_point(self.max_iter)
-                    self.announce_point(self.avg_iter)
+                    self.min_iter.set_point(float(i), min(self.min_iter.y, min(a.path_len for a in ants)))
+                    self.max_iter.set_point(float(i), max(a.path_len for a in ants))
+                    self.avg_iter.set_point(float(i), sum(a.path_len for a in ants) / len(ants))
+                    self.min_iter.announce()
+                    self.max_iter.announce()
+                    self.avg_iter.announce()
 
                     self.S.update_pheromone(ants)
                     self.S.daemon_actions()
@@ -54,7 +56,7 @@ class AntSolver(Plottable):
                     else:
                         repeats = 0
                     i = i + 1
-        except Exception as e:
+        except Exception as _:
             traceback.print_exc()
             log()['SOLVER'].log('Something bad happened, saving results...')
         finally:

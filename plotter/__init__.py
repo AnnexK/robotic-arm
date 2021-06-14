38658f725408fd0d62d57701821d80df2994a046
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 19:13:53 +0700

    Mux removal

--- plotter/__init__.py
+++ plotter/__init__.py
@@ -1,6 +1,7 @@
-from .mux import Mux, PMLink,  MPLink
 from .plottable import Plottable
 from .plotter import Plotter
 
 from .mpl_plotter import MPLPlotter
 from .csv_writer import CSVWriter
+
+from .null import NullPlotter

4fd92aa77098cd8b4993503853f003d4c6e1beda
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 21:06:14 +0700

    now only shows sdfs

--- env-viewer.py
+++ env-viewer.py
@@ -1,33 +1,20 @@
-#!/bin/python3
-
 import argparse
-from env import Environment
+import pybullet as pb
 import pathlib as path
 
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('sdf')
-    p.add_argument('urdf')
-    p.add_argument('--state', default=None)
     args = p.parse_args()
-
-    if args.state is None:
-        state = None
-    else:
-        state = list(float(s) for s in args.state.split(';'))
 
     sdffile = str(path.Path(args.sdf).resolve())
     print(sdffile)
-    E = Environment(filename=sdffile)
-    urdffile = str(path.Path(args.urdf).resolve())
-    E.add_robot(filename=urdffile,
-                eff_name='effector')
-    if state is not None:
-        E.robot.state = state
+
+    pb.connect(pb.GUI) # type: ignore
+    pb.loadSDF(sdffile) # type: ignore
 
     input()
-    del E
 
 
 if __name__ == '__main__':

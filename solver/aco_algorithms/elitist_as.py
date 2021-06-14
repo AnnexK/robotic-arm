9e2d5d494901a2efcb157b8776ebbbae17ea341e
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 19:17:29 +0700

    Typing

--- solver/aco_algorithms/elitist_as.py
+++ solver/aco_algorithms/elitist_as.py
@@ -1,12 +1,20 @@
 from .ant_system import AntSystem
 from logger import log
+from typing import TypeVar, Iterable
+from graphs import PhiGraph
+from ..daemons import Daemon
+from ..ants import BaseAnt
 
-class ElitistAS(AntSystem):
-    def __init__(self, G, Q, decay, limit, daemon, Q_e):
+
+V = TypeVar('V')
+
+
+class ElitistAS(AntSystem[V]):
+    def __init__(self, G: PhiGraph[V], Q: float, decay: float, limit: int, daemon: Daemon, Q_e: float):
         super().__init__(G, Q, decay, limit, daemon)
         self.elite_power = Q_e
 
-    def update_pheromone(self, ants):
+    def update_pheromone(self, ants: Iterable[BaseAnt[V]]):
         super().update_pheromone(ants)
         L = self.best_length
         phi = self.elite_power / L

e2db65b21f99fb71b2b06d6745607945ed1f03f3
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 19:03:41 +0700

    Typing, removed debug msg

--- graphs/prm/prm.py
+++ graphs/prm/prm.py
@@ -1,21 +1,25 @@
+from typing import Sequence, Tuple, List
+from env.types import State
 import numpy.random as random
 from math import inf
+from .metrics import Metric
+from .prmgraph import PRMGraph
+from env.robot import Robot
 
 
 class PRM:
-    def __init__(self, n, metric, robot, phi):
+    def __init__(self, n: int, metric: Metric, robot: Robot, phi: float):
         self.n = n
         self.metric = metric
         self.robot = robot
-        self.rng = random.default_rng()
+        self.rng: random.Generator = random.default_rng()
         self.nsteps = 25
         self.phi = phi
 
-    def generate_vertex(self):
+    def generate_vertex(self) -> State:
         size = len(self.robot.state)
         while True:
             s = self.rng.random(size)
-            print(s)
             v = tuple(
                 (u-l) * s[i] + l for i, (u, l) in enumerate(
                     zip(
@@ -24,34 +28,30 @@ class PRM:
                     )
                 )
             )
-            print(v)
             self.robot.state = v
             if not self.robot.check_collisions():
                 return v
 
-    def find_nearest(self, graph, v):
+    def find_nearest(self, graph: PRMGraph, v: State) -> Sequence[State]:
         M = [(m, self.metric(v, m)) for m in graph.vertices if v != m]
-        ret = []
+        ret: List[State] = []
 
-        def order(x):
-            # print(x)
-            if x in ret:
+        def order(x: Tuple[State, float]):
+            if x[0] in ret:
                 return inf
             else:
                 return x[1]
         
-        for i in range(self.n):
-            print(i)
-            print(ret)
-            ret.append( min( M, key=order ) )
-        return [r[0] for r in ret]
+        for _ in range(self.n):
+            ret.append( min( M, key=order )[0] )
+        return ret
 
-    def try_connect(self, graph, v, w):
+    def try_connect(self, graph: PRMGraph, v: State, w: State):
         diff = tuple( (wc-vc)/self.nsteps for wc, vc in zip(w,v) )
-        def next_config(cur):
+        def next_config(cur: State) -> State:
             return tuple(c+d for c, d in zip(cur, diff))
         cur = next_config(v)
-        for i in range(self.nsteps):
+        for _ in range(self.nsteps):
             self.robot.state = cur
             if self.robot.check_collisions():
                 return

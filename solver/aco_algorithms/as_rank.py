e558637e0a9384f528a2d3651af99c638c8b5520
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 19:17:18 +0700

    Typing

--- solver/aco_algorithms/as_rank.py
+++ solver/aco_algorithms/as_rank.py
@@ -1,21 +1,28 @@
+from solver.daemons.daemon import Daemon
+from graphs.phigraph import PhiGraph
+from typing import Iterable, TypeVar
 from .ant_system import AntSystem
+from ..ants import BaseAnt
 
 
-class ASRank(AntSystem):
-    def __init__(self, G, Q, decay, limit, rank_num, daemon):
+V = TypeVar('V')
+
+
+class ASRank(AntSystem[V]):
+    def __init__(self, G: PhiGraph[V], Q: float, decay: float, limit: int, rank_num: int, daemon: Daemon):
         super().__init__(G, Q, decay, limit, daemon)
         self.rank_n = rank_num
 
-    def update_pheromone(self, ants):
-        self.graph.evaporate(self.decay)
-
-        ants.sort(key=lambda a: a.path_len)
+    def update_pheromone(self, ants: Iterable[BaseAnt[V]]):
+        self.graph.evaporate(self.rate)
+        seq = list(ants)
+        seq.sort(key=lambda a: a.path_len)
 
         # если муравьев меньше, чем рангов,
         # то использовать количество муравьев
         # вместо количества рангов
-        ranked_ants = min(self.rank_n, len(ants))
+        ranked_ants = min(self.rank_n, len(seq))
 
         for r in range(ranked_ants):
             phi = self.ant_power * (ranked_ants - r)
-            ants[r].deposit_pheromone(phi)
+            seq[r].deposit_pheromone(phi)

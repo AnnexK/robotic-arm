a0915481360a57a63a2b214e0ca7f89010cdc38e
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-14 19:16:57 +0700

    typing

--- solver/aco_algorithms/ant_system.py
+++ solver/aco_algorithms/ant_system.py
@@ -2,27 +2,34 @@ import numpy.random as random
 from math import inf
 from logger import log
 from copy import deepcopy
+from typing import TypeVar, List, Iterable, Sequence
+from .algorithm import ACOAlgorithm
+from ..daemons import Daemon
+from graphs import PhiGraph
+from ..ants import BaseAnt
 
 
-class AntSystem:
-    def __init__(self, G, Q, decay, limit, daemon):
+V = TypeVar('V')
+
+
+class AntSystem(ACOAlgorithm[V]):
+    def __init__(self, G: PhiGraph[V], Q: float, decay: float, limit: int, daemon: Daemon):
         random.seed()
         self.graph = G
         self.ant_power = Q
         self.rate = decay
         # последовательность вершин лучшего решения
-        self.best_solution = []
+        self.best_solution: List[V] = []
         # длина лучшего решения
         self.best_length = inf
-        self.limit = inf if limit == 0 else limit
+        self.limit = limit
         self.daemon = daemon
         
-    def generate_solutions(self, ants):
-        lens = []
+    def generate_solutions(self, ants: Iterable[BaseAnt[V]]):
         for i, a in enumerate(ants):
-            steps = 0
+            steps: int = 0
             log()['ANT'].log('Ant #{}'.format(i+1))
-            while not a.complete and steps < self.limit:
+            while not a.complete and self.limit and steps < self.limit:
                 a.pick_edge()
                 steps += 1
                 if steps % 10000 == 0:
@@ -32,7 +39,6 @@ class AntSystem:
                 log()['ANT'].log(f'Ant #{i+1} hit the limit, aborting...')
             else:
                 length = a.path_len
-                lens.append(length)
                 log()['ANT'].log('Ant #{} finished'.format(i+1))
                 log()['ANT'].log('Path length: {}'.format(length))
                 if length < self.best_length:
@@ -41,10 +47,10 @@ class AntSystem:
             # вернуться к состоянию до итерации
             a.reset()
 
-    def result(self):
+    def result(self) -> Sequence[V]:
         return self.best_solution
 
-    def update_pheromone(self, ants):
+    def update_pheromone(self, ants: Iterable[BaseAnt[V]]):
         # испарение
         self.graph.evaporate(self.rate)
         # отложение

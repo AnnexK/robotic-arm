dd67f3f69040444430b28e775334af1e7eec4665
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-16 21:23:49 +0700

    Environment now loads tasks in separate method

--- env/__init__.py
+++ env/__init__.py
@@ -1,5 +1,4 @@
 import json
-import pathlib
 
 from env.environment import Environment
 from .taskdata import TaskData
@@ -9,20 +8,14 @@ from logger import log
 from .taskdata import TaskData, task_json_parse
 
 
-def load_task(filename: pathlib.Path, render: bool, fallback: bool) -> Environment:
+def load_task(filename: str, render: bool, fallback: bool) -> Environment:
     """Загружает параметры задачи из json-файла filename"""
     with open(filename, 'r') as fp:
         td: TaskData = json.load(fp, object_hook=task_json_parse)
 
-    # получение путей к urdf и sdf файлам
-
-    td.urdf = str(filename.parent / pathlib.Path(td.urdf))
-    if td.sdf:
-        td.sdf = str(filename.parent / pathlib.Path(td.sdf))
-
     E = Environment(render,
-                    fallback,
-                    td)
+                    fallback)
+    E.load_task(td)
 
     log()['PYBULLET'].log('environment loaded successfully')
 
--- env/environment.py
+++ env/environment.py
@@ -1,7 +1,7 @@
 import pybullet as pb
 from .robot import Robot
 
-from typing import List
+from typing import List, Optional
 from .types import Vector3
 from .taskdata import TaskData
 
@@ -10,7 +10,7 @@ class Environment:
     """Обертка над объектами pybullet, содержащая информацию
 об объектах внешней среды и предназначенная для хранения информации
 и удобного освобождения ресурсов"""
-    def __init__(self, render: bool, fallback: bool, td: TaskData):
+    def __init__(self, render: bool, fallback: bool):
         """Параметры:
 filename -- имя SDF-файла (абсолютный путь)
 при отсутствии параметра создается пустая внешняя среда"""
@@ -18,37 +18,52 @@ filename -- имя SDF-файла (абсолютный путь)
             pb.GUI if render else pb.DIRECT, #type: ignore
             options='--opengl2' if fallback else ''
         )
-        if not td.sdf:
-            self._ids: List[int] = list()  # пустой
-        else:
-            try:
-                self._ids = pb.loadSDF(td.sdf, #type: ignore
-                                       physicsClientId=self.s)
-            except pb.error: #type: ignore
-                raise ValueError('failed to read SDF file')
+        self._ids: List[int] = list()
 
-        self.endpoint_obj: int = self.set_endpoint(td.target_pos)
-        self.endpoint: Vector3 = td.target_pos
-        self._robot = Robot(self.s, td)
+        self.endpoint_obj: int = -1
+        self.endpoint: Vector3 = (0.0, 0.0, 0.0)
+        self._robot: Optional[Robot] = None
 
     def __del__(self):
         """Предназначено для освобождения ресурсов"""
-        for i in self._ids:
-            pb.removeBody(i) #type: ignore
-        pb.removeBody(self.endpoint_obj) #type: ignore
-        del self._robot
+        self._remove_bodies()
         pb.disconnect(self.s) #type: ignore
 
+    def _remove_bodies(self):
+        for i in self._ids:
+            pb.removeBody(i, physicsClientId=self.s) #type: ignore
+        self._ids.clear()
+        if self.endpoint_obj >= 0:
+            pb.removeBody(self.endpoint_obj, physicsClientId=self.s) #type: ignore
+            self.endpoint_obj = -1
+        self._robot = None
+    
+    def __bool__(self) -> bool:
+        return self._robot is not None
+    
+    def load_task(self, td: TaskData):
+        self._remove_bodies()
+        if td.sdf:
+            try:
+                self._ids = list(pb.loadSDF(td.sdf, #type: ignore
+                                            physicsClientId=self.s))
+            except pb.error: #type: ignore
+                raise ValueError('failed to read SDF file')
+
+        self.endpoint_obj = self.set_endpoint(td.target_pos)
+        self.endpoint: Vector3 = td.target_pos
+        self._robot = Robot(self.s, td)
+    
     def set_endpoint(self, p: Vector3) -> int:
         visual: int = pb.createVisualShape(shapeType=pb.GEOM_SPHERE, #type: ignore
                                            radius=0.05,
-                                           rgbaColor=[1., 0., 0., 1.])
+                                           rgbaColor=[1., 0., 0., 1.], physicsClientId=self.s)
         mb: int = pb.createMultiBody(baseVisualShapeIndex=visual, #type: ignore
-                                     basePosition=p)
+                                     basePosition=p, physicsClientId=self.s)
         return mb
 
     @property
-    def robot(self) -> Robot:
+    def robot(self) -> Optional[Robot]:
         return self._robot
 
     @property

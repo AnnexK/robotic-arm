343eeae9fdb7cfa4f08c37d53268b2f9007dd34d
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-06-23 02:35:55 +0700

    Continuous joints implementation

--- graphs/prm/metrics/euclid.py
+++ graphs/prm/metrics/euclid.py
@@ -2,11 +2,20 @@ from env.types import State
 from math import sqrt
 from .metric import Metric
 from env.types import State
+from env.robot import Robot, JointType
+from math import pi
 
 
 class EuclideanMetric(Metric):
-    def __init__(self):
-        pass
+    def __init__(self, R: Robot):
+        self.types = R.types
 
     def __call__(self, A: State, B: State) -> float:
-        return sqrt( sum( (a-b)*(a-b) for a, b in zip(A, B) ) )
+        def diffval(a: float, b: float, t: JointType):
+            if t is JointType.CONTNIUOUS:
+                diff = abs(a-b)
+                return min(diff, 2*pi-diff)
+            else:
+                return a-b
+            
+        return sqrt( sum( diffval(a, b, t)**2 for a, b, t in zip(A, B, self.types) ) )
